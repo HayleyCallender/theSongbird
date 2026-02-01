ac5d4130a19f923ff656cf4104242a91ebd107b9
Hayley Callender <118288349+Songbird1978@users.noreply.github.comgit>
2026-02-01 15:23:32 +0000

    changed about info

--- client/src/pages/hotspots/about.jsx
+++ client/src/pages/hotspots/about.jsx
@@ -19,7 +19,7 @@ function About() {
                         "I have learned through necessity to design assets and graphic elements for my web design projects and musical promotion and marketing.",
                         "The projects that you can view in Designs use Adobe Creative Suite and mostly Photoshop, InDesign and Illustrator but I have also created videos using Premier Pro.",
                         "I have included these projects to showcase my skills that are beneficial to website development and design.",
-                        "I like to mix drawings or photographed elements with graphics. I like the textures that can create. My plan for this portfolio is to develop some drawings for the town and backgrounds.  I have also used some AI to create images for backgrounds but I would like to replace those with handmade elements."
+                        "I like to mix drawings or photographed elements with graphics. I like the textures that can create. My plan for this portfolio is to develop some drawings for the town and backgrounds. "
                    
                     ]}
                     speed={20}
